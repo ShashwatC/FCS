bbb2854c201c52e653e053fce6b9f958b0fbdbed
ShashwatC <shashwat15091@iiitd.ac.in>
2018-10-30 19:31:00 +0530

    Preparing to merge

--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,12 @@
+from django.http import Http404
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
-	return render(request, 'customer/index.html')
+    user = request.user
+    if user.groups.count() == 1:
+        raise Http404("Poll does not exist")
+
+    group_stored = int(user.groups.values('name').first()['name'])
+    return render(request, 'customer/index.html')
