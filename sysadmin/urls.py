584b223f89d514222211cd0ef8d5b1cb36e3223e
Niket100 <niket16060@iiitd.ac.in>
2018-11-01 22:28:46 +0530

    sysadmin commit

--- sysadmin/urls.py
+++ sysadmin/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('pending', views.pending, name='pending requests'),
+    path('removal', views.del_internal, name='del_internal'),
+    path('view', views.view_trans, name='view_trans'),
+    path('approv', views.acc_pen, name='approv'),
+    
 ]
 
